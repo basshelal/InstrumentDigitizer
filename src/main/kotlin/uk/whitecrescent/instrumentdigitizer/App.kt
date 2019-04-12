36d6488c36c66bd28b1f1c3ca7b80ed93f75fae7
Bassam Helal <basshelal@gmail.com>
2019-04-12 23:56:30 +0100

    Removed JSyn files

--- src/main/kotlin/uk/whitecrescent/instrumentdigitizer/App.kt
+++ src/main/kotlin/uk/whitecrescent/instrumentdigitizer/App.kt
@@ -1,11 +1,5 @@
 package uk.whitecrescent.instrumentdigitizer
 
-import com.jsyn.JSyn
-import com.jsyn.devices.javasound.MidiDeviceTools
-import com.jsyn.instruments.DualOscillatorSynthVoice
-import com.jsyn.midi.MidiSynthesizer
-import com.jsyn.unitgen.LineOut
-import com.jsyn.util.MultiChannelSynthesizer
 import javafx.application.Application
 import javafx.stage.Stage
 import sun.audio.AudioDevice
@@ -16,8 +10,6 @@ import javax.sound.midi.MidiUnavailableException
 import javax.sound.midi.Receiver
 import javax.sound.midi.ShortMessage
 import javax.sound.midi.Synthesizer
-import javax.sound.sampled.AudioSystem
-import kotlin.concurrent.thread
 
 class Synth {
 
@@ -66,85 +58,6 @@ class Synth {
                 if (isOpen) AudioDevice.device.close()
                 println("${AudioDevice.device} is closed")
             }
-        }
-    }
-}
-
-class UseMidiKeyboard {
-
-    init {
-        val synth = JSyn.createSynthesizer()
-
-        val voiceDescription = DualOscillatorSynthVoice.getVoiceDescription()
-
-        val multiSynth = MultiChannelSynthesizer()
-        multiSynth.setup(synth, 0, 16, 16, voiceDescription)
-        val midiSynthesizer = MidiSynthesizer(multiSynth)
-
-        // Create a LineOut for the entire synthesizer.
-        val lineOut = LineOut()
-        synth!!.add(lineOut)
-        multiSynth.output.connect(0, lineOut.input, 0)
-        multiSynth.output.connect(1, lineOut.input, 1)
-
-        // Start synthesizer using default stereo output at 44100 Hz.
-        synth.start()
-        lineOut.start()
-
-        val format = EASY_FORMAT
-        val line = AudioSystem.getSourceDataLine(format)
-
-        line.apply {
-            open(format)
-            start()
-        }
-
-        val keyboard = MidiDeviceTools.findKeyboard()
-        // Just use default synthesizer.
-        if (keyboard != null) {
-            // If you forget to open them you will hear no sound.
-            keyboard.open()
-            // Put the receiver in the transmitter.
-            // This gives fairly low latency playing.
-            println("Play MIDI keyboard: ${keyboard.deviceInfo.description}")
-
-            keyboard.transmitter.receiver = object : Receiver {
-                override fun close() {
-                    print("Closed.")
-                }
-
-                override fun send(message: MidiMessage, timeStamp: Long) {
-                    require(message is ShortMessage)
-                    when (message.command) {
-                        ShortMessage.NOTE_ON -> {
-                            val key = Key.fromNumber(message.data1)
-                            val buffer = BASIC_INSTRUMENT.getByteArray(key.frequency, 1.0, 1.0)
-                            thread {
-                                line.write(buffer, 0, buffer.size)
-                            }
-
-                            println("TimeStamp: $timeStamp")
-                            println("Channel: ${message.channel}")
-                            println("Command: ${message.command}")
-                            println("Data1 (Note): ${message.data1}") // Note value
-                            println("Data2 (Vel) : ${message.data2}") // Velocity
-                            println("$key  ${key.frequency}")
-                        }
-                        ShortMessage.NOTE_OFF -> {
-                            line.flush()
-                        }
-                        ShortMessage.PITCH_BEND -> {
-                            println("Pitch Bend")
-                            println("Command: ${message.command}")
-                            println("Data1: ${message.data1}") // Note value
-                            println("Data2: ${message.data2}") // Velocity
-                        }
-                    }
-
-                }
-            }
-        } else {
-            println("Could not find a keyboard.")
         }
     }
 }
--- src/test/kotlin/tests/RandomTests.kt
+++ src/test/kotlin/tests/RandomTests.kt
@@ -1,12 +1,5 @@
 package tests
 
-import com.jsyn.JSyn
-import com.jsyn.Synthesizer
-import com.jsyn.unitgen.LineOut
-import com.jsyn.unitgen.SineOscillator
-import com.jsyn.unitgen.UnitGenerator
-import com.jsyn.unitgen.UnitVoice
-import com.softsynth.shared.time.TimeStamp
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
@@ -34,7 +27,6 @@ import uk.whitecrescent.instrumentdigitizer.generateIntSineWave
 import uk.whitecrescent.instrumentdigitizer.generateSineWave
 import uk.whitecrescent.instrumentdigitizer.generateTwoSineWaves
 import uk.whitecrescent.instrumentdigitizer.getFrequenciesDistinct
-import uk.whitecrescent.instrumentdigitizer.getSineOscillators
 import uk.whitecrescent.instrumentdigitizer.i
 import uk.whitecrescent.instrumentdigitizer.inverseFourierTransform
 import uk.whitecrescent.instrumentdigitizer.label
@@ -73,114 +65,6 @@ import kotlin.math.hypot
 
 @DisplayName("Random Tests")
 class RandomTests {
-
-    @DisplayName("Test JSyn Synth")
-    @Test
-    fun testJSynSynth() {
-        val synth: Synthesizer
-        val ugen: UnitGenerator
-        val voice: UnitVoice
-        val lineOut: LineOut
-
-
-        // Create a context for the synthesizer.
-        synth = JSyn.createSynthesizer()
-        // Set output latency to 123 msec because this is not an interactive app.
-        synth.audioDeviceManager.setSuggestedOutputLatency(0.123)
-
-        // Add a tone generator.
-        ugen = SineOscillator()
-        synth.add(ugen)
-        voice = ugen
-        // Add an output mixer.
-        lineOut = LineOut()
-        synth.add(lineOut)
-
-        // Connect the oscillator to the left and right audio output.
-        voice.output.connect(0, lineOut.input, 0)
-        voice.output.connect(0, lineOut.input, 1)
-
-        // Start synthesizer using default stereo output at 44100 Hz.
-        synth.start()
-
-        // Get synthesizer time in seconds.
-        val timeNow = synth.currentTime
-
-        // Advance to a near future time so we have a clean start.
-        var timeStamp = TimeStamp(timeNow + 0.5)
-
-        // We only need to start the LineOut. It will pull data from the
-        // oscillator.
-        synth.startUnit(lineOut, timeStamp)
-
-        // Schedule a note on and off.
-        var freq = 200.0 // hertz
-        val duration = 1.4
-        val onTime = 1.0
-        voice.noteOn(freq, 0.5, timeStamp)
-        voice.noteOff(timeStamp.makeRelative(onTime))
-
-        // Schedule this to happen a bit later.
-        timeStamp = timeStamp.makeRelative(duration)
-        freq *= 1.5 // up a perfect fifth
-        voice.noteOn(freq, 0.5, timeStamp)
-        voice.noteOff(timeStamp.makeRelative(onTime))
-
-        timeStamp = timeStamp.makeRelative(duration)
-        freq *= 4.0 / 5.0 // down a major third
-        voice.noteOn(freq, 0.5, timeStamp)
-        voice.noteOff(timeStamp.makeRelative(onTime))
-
-        // Sleep while the song is being generated in the background thread.
-        try {
-            println("Sleep while synthesizing.")
-            synth.sleepUntil(timeStamp.time + 2.0)
-            println("Woke up...")
-        } catch (e: InterruptedException) {
-            e.printStackTrace()
-        }
-
-        // Stop everything.
-        synth.stop()
-
-    }
-
-    @DisplayName("Test JSyn Synth 2")
-    @Test
-    fun testJSynSynth2() {
-        val synth = JSyn.createSynthesizer()
-        val lineOut = LineOut()
-        synth.add(lineOut)
-
-        val oscillators = getSineOscillators(10)
-        oscillators.forEach { synth.add(it) }
-        oscillators.forEach {
-            it.output.connect(0, lineOut.input, 0)
-            it.output.connect(0, lineOut.input, 1)
-        }
-        lineOut.start()
-        synth.start()
-
-        oscillators.onEach { it.noteOn(220.0, 0.1) }
-
-        Thread.sleep(2000)
-
-        oscillators.onEach {
-            it.noteOff()
-            it.noteOn(440.0, 0.1)
-        }
-
-        Thread.sleep(2000)
-
-        oscillators.onEach {
-            it.noteOff()
-            it.noteOn(880.0, 0.1)
-        }
-
-        Thread.sleep(2000)
-
-        synth.stop()
-    }
 
     @DisplayName("Test Write Data")
     @Test
